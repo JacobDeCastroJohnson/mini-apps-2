0f37b18535bc8dd7b42963f39028fa0fb483faf2
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-11-06 13:39:45 -0800

    Initialize development environment

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+    //THIS BINDING AREA
+  }
+
+  render() {
+    return(
+      <h1> Test from the front end!</h1>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
 
